1f0be1fb38933cecf11b5d6b78b587d1482bf8d7
jason_chung <jason_chung@17life.com>
2019-12-21 00:40:36 +0800

    Initial Commit

---
+++ Assignment/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Assignment
+//
+//  Created by Jason_Chung on 2019/12/21.
+//  Copyright © 2019 Jason_Chung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
